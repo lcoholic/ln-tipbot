687263aae2420f9594bb6f4f47f980ebfdd6d1ca
lcoholic <beginner_MC@web,de>
2023-04-13 16:07:02 +0200

    help message to config

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 BOT_TOKEN = "MyBotToken"
 RPC_PATH = "MyRpcPath"
+HELP_MSG = "This is the tip bot of https://t.me/l_coholic. Just enter the amount of sats you would like to donate." 
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import qrcode
 from pyln.client import LightningRpc
 from aiogram import Bot, Dispatcher, executor, types
 
-from config import BOT_TOKEN, RPC_PATH
+from config import BOT_TOKEN, RPC_PATH, HELP_MSG
 
 logging.basicConfig(filename='bot.log', encoding='utf-8', level=logging.INFO,
                     format='%(asctime)s : %(module)s : %(message)s')
@@ -15,11 +15,9 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 l = LightningRpc(RPC_PATH)
 
-help_message = f'This is the tip bot of https://t.me/l_coholic. Just enter the amount of sats you would like to donate.' \
-
 @dp.message_handler(commands=['start', 'help', 'h'])
 async def welcome(message: types.Message):
-    await message.answer(help_message)
+    await message.answer(HELP_MSG)
 
 
 @dp.message_handler()
@@ -31,7 +29,7 @@ async def answer_invoice_qrcode(message: types.Message):
         amountmSat = int(amountSatMatch.group()) * 1000
         now = time.time()
         invoice = l.invoice(amount_msat=amountmSat, label='lbl{}'.format(now), 
-                         description=f'from {message.from_user.username} via al_tipbot')
+                         description=f'from {message.from_user.username} via my tip bot')
         bolt11 = invoice['bolt11']
         qrcode.make(bolt11).save('qrcode.png')
         await message.answer_photo(photo=open('qrcode.png', 'rb'), caption=bolt11)
